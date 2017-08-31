2be7fbf10d05655fcaf61ba4a7c84609b966a726
17868804701 <742210203@qq.com>
2017-08-31 15:36:58 +0800

    修改

--- sendEmail.js
+++ sendEmail.js
@@ -2,15 +2,15 @@ const nodemailer = require('nodemailer');
 const transporter = nodemailer.createTransport({
     service: 'qq',
     auth: {
-        user: '742210203@qq.com',//发送者邮箱
-        pass: 'hqqmryrzveigbfej' //授权码,通过QQ获取
+        user: '******@qq.com',//发送者邮箱
+        pass: '******' //授权码,通过QQ获取
     }
 });
 
 const mailOptions = {
-    from: '742210203@qq.com', // 发送者
-    to: 'hs951013@163.com', // 接受者,可以同时发送多个,以逗号隔开
-    cc: ',17868804701@163.com',//抄送
+    from: '******@qq.com', // 发送者
+    to: '******@163.com', // 接受者,可以同时发送多个,以逗号隔开
+    cc: '******@163.com',//抄送
     subject: '发送邮件测试', // 标题
     text: 'Hello world', // 文本
     html: `<h2>NodeJS发送邮件测试</h2>`
